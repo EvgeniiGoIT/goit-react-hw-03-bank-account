02e333a01e08f06f5d65b0b4f90c060aba516222
Evgenii <wojda007@gmail.com>
2019-11-03 20:54:06 +0200

    Updates

---
+++ precache-manifest.80de109a943475f991b900dc7663aceb.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40c2032ff08550fa5f782060b601411d",
+    "url": "/goit-react-hw-02-bank-account/index.html"
+  },
+  {
+    "revision": "d10df919783fee1453db",
+    "url": "/goit-react-hw-02-bank-account/static/js/2.dcad576e.chunk.js"
+  },
+  {
+    "revision": "39900502fb877c086911",
+    "url": "/goit-react-hw-02-bank-account/static/js/main.e09083b9.chunk.js"
+  },
+  {
+    "revision": "213fa550274cb66f14de",
+    "url": "/goit-react-hw-02-bank-account/static/js/runtime-main.5ebd1f8e.js"
+  }
+]);
